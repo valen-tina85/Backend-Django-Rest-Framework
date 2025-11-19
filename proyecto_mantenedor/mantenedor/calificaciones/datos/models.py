19e71f84617b2122f0f31fcc429d9c764cbb3a6a
valentina.h <valentina.hernandez.h23@hotmail.com>
2025-11-19 15:19:54 -0300

    Actualizacion tabla calificacion

--- proyecto_mantenedor/mantenedor/calificaciones/datos/models.py
+++ proyecto_mantenedor/mantenedor/calificaciones/datos/models.py
@@ -15,10 +15,22 @@ class Mercado(models.Model):
         return self.nombre
 
 class Calificacion(models.Model):
-    ejercicio = models.IntegerField()
+    ejercicio = models.IntegerField(null=False)
     instrumento = models.CharField(max_length=3)
-    fecha_pago = models.DateField()
+    fecha_pago = models.DateField(blank=True, null=True)
     origen = models.ForeignKey(Origen, on_delete=models.CASCADE, related_name='origenCalificacion')
     mercado = models.ForeignKey(Mercado, on_delete=models.CASCADE, related_name='mercadoCalificacion')
+    periodo = models.IntegerField(default=0,null=False)
+    secuencia_evento = models.IntegerField(default=0, null=False)
+    factor_actualizacion = models.IntegerField(default=0, null=False)
+    dividendo = models.IntegerField(default=0, null=False)
+    valor_historico = models.FloatField(default=0.0, null=False)
+    fechaActualizacion = models.DateField(blank=True, null=True)
+    anio = models.DateField(blank=True, null=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.instrumento
+
 
  
