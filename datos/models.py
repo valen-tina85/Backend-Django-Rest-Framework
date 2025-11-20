28656822a045b0beb3701aebe806f5befef73455
valentina-h <valentina.hernandez.h23@hotmail.com>
2025-11-19 22:38:44 -0300

    Agrego conexión a mysql

--- datos/models.py
+++ datos/models.py
@@ -16,21 +16,24 @@ class Mercado(models.Model):
 
 class Calificacion(models.Model):
     ejercicio = models.IntegerField(null=False)
-    instrumento = models.CharField(max_length=3)
-    fecha_pago = models.DateField(blank=True, null=True)
-    origen = models.ForeignKey(Origen, on_delete=models.CASCADE, related_name='origenCalificacion')
-    mercado = models.ForeignKey(Mercado, on_delete=models.CASCADE, related_name='mercadoCalificacion')
+    instrumento = models.CharField(max_length=3, blank=False, null=False)
+    fecha_pago = models.DateField(null=False)
+    origen = models.ForeignKey(Origen, on_delete=models.PROTECT, related_name='origen_calificacion')
+    mercado = models.ForeignKey(Mercado, on_delete=models.PROTECT, related_name='mercado_calificacion')
     periodo = models.IntegerField(default=0,null=False)
     secuencia_evento = models.IntegerField(default=0, null=False)
     factor_actualizacion = models.IntegerField(default=0, null=False)
     dividendo = models.IntegerField(default=0, null=False)
     valor_historico = models.FloatField(default=0.0, null=False)
-    fechaActualizacion = models.DateField(blank=True, null=True)
-    anio = models.DateField(blank=True, null=True)
+    fechaActualizacion = models.DateField(null=False)
+    anio = models.DateField("Año", null=False)
     descripcion = models.TextField(blank=True, null=True)
 
+    class Meta:
+        ordering = ["-instrumento","periodo"]
+
     def __str__(self) -> str:
-        return self.instrumento
+        return f"Calificación {self.instrumento} - {self.ejercicio}"
 
 
  
